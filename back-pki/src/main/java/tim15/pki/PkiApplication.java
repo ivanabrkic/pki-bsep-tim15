3a846311e01ba557719de36e69d55ab529eee2c0
Ivana Brkić <ivanabrkic192@gmail.com>
2020-04-02 17:54:34 +0200

    First commit - added Spring Boot and Angular starter app.

---
+++ back-pki/src/main/java/tim15/pki/PkiApplication.java
@@ -0,0 +1,13 @@
+package tim15.pki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiApplication.class, args);
+	}
+
+}
