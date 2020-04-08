f799ca77a400fd24eeb1404c85269746850f8e93
lukicMilan <lukic.milan22@gmail.com>
2020-04-08 10:15:57 +0200

    error solving

--- back-pki/src/main/java/tim15/pki/repository/CertificateRepository.java
+++ back-pki/src/main/java/tim15/pki/repository/CertificateRepository.java
@@ -1,18 +1,20 @@
 package tim15.pki.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import tim15.pki.model.Certificate;
 import java.util.List;
 
 //FALI extends JpaRepository<Certificate, Long>
 //U KRAJNJEM SLUCAJU TREBA GA CELOG IMPLEMENTIRATI
-public interface CertificateRepository {
+public interface CertificateRepository extends JpaRepository<Certificate, Long> {
     List<Certificate> findAll();
     Certificate findOneBySerialNumber(String serialNumber);
-    List<Certificate> findAllByName(String name);
     Certificate save(Certificate certificate);
     void removeBySerialNumber(String serialNumber);
 
-    //@Query("select c from Certificate c where c.revokeReason != null")
+
     //PROVERITI upit!!
+    @Query("select c from Certificate c where c.revokeReason is not null")
     List<Certificate> findRevokedCertificates();
 }
