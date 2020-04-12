5269b86dbd50a8eb05781ddae26706e0500190b1
lukicMilan <lukic.milan22@gmail.com>
2020-04-12 21:21:40 +0200

    dodato brisanje iz keystora

--- back-pki/src/main/java/tim15/pki/service/CertificateRevokeService.java
+++ back-pki/src/main/java/tim15/pki/service/CertificateRevokeService.java
@@ -1,17 +1,27 @@
 package tim15.pki.service;
 
+import tim15.pki.dto.CertificateGenDTO;
+import tim15.pki.dto.TextMessage;
 import tim15.pki.model.Certificate;
 import tim15.pki.model.enums.CertificateStatus;
 import tim15.pki.model.enums.RevokeReason;
 import tim15.pki.repository.CertificateRepository;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.cert.CertificateException;
 import java.util.Collection;
 
 public class CertificateRevokeService {
      CertificateRepository certificateRepository;
      LoggerService loggerService;
 
-     public void revokeCertificate(String serialNumber, RevokeReason revokeReason) {
+     public TextMessage revokeCertificate(String serialNumber, RevokeReason revokeReason) {
          Certificate certificate = certificateRepository.findOneBySerialNumber(serialNumber);
 
          if(certificate.getCertificateStatus() != CertificateStatus.REVOKED) {
@@ -20,6 +30,7 @@ public class CertificateRevokeService {
              certificate.setCertificateStatus(CertificateStatus.REVOKED);
              Collection<Certificate> issuedCertificates = certificate.getIssuerToCertificates();
              for (Certificate issuedCertificate : issuedCertificates) {
+                 this.revokeFromKeyStore(issuedCertificate.getSerialNumber());
                  switch (issuedCertificate.getRevokeReason()) {
                      case EXPIRED:
                          revokeCertificate(issuedCertificate.getSerialNumber(), RevokeReason.CERTIFICATE_HOLD);
@@ -34,7 +45,25 @@ public class CertificateRevokeService {
              }
 
              certificateRepository.save(certificate);
+             this.revokeFromKeyStore(certificate.getSerialNumber());
              loggerService.print("Certificate " + serialNumber + " successfully revoked.");
+             return new TextMessage("Certificate " + serialNumber + " successfully revoked.");
          }
-     }
+         else {
+             loggerService.print("Certificate " + serialNumber + " revocation failed.");
+             return new TextMessage("Certificate " + serialNumber + " revocation failed..");
+         }
+        }
+
+        private void revokeFromKeyStore(String serialNumber) {
+            String alias = null;
+            try {
+                KeyStore keyStore = KeyStore.getInstance("JKS", "SUN");
+                keyStore.deleteEntry(serialNumber);
+            } catch (KeyStoreException e) {
+                e.printStackTrace();
+            } catch (NoSuchProviderException e) {
+                loggerService.print(e.getMessage());
+            }
+    }
 }
